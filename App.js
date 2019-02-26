61a077898a06b8a518c079be9cb5113c26ad3763
Anny Guttierrez <agutilop@everis.com>
2019-02-26 15:17:56 -0500

    Add openDrawerNavigation
    - Add open DrawerNavigator button.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, Dimensions } from 'react-native';
-import {createDrawerNavigator, createAppContainer, DrawerItems} from 'react-navigation';
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView, Dimensions } from 'react-native';
+import { createDrawerNavigator, createAppContainer, DrawerItems } from 'react-navigation';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 
@@ -12,10 +12,23 @@ export default class App extends React.Component {
   }
 }
 
+const CustomDrawerComponent = (props) => (
+  <SafeAreaView style={{ flex: 1 }}>
+    <View>
+      <Image source={require('./assets/icon.png')} style={{ height: 120, width: 120 }}/>
+    </View>
+    <ScrollView>
+      <DrawerItems {...props} />
+    </ScrollView>
+  </SafeAreaView>
+);
+
 const AppDrawerNavigator = createDrawerNavigator({
   Home: HomeScreen,
   Settings: SettingsScreen
-});
+}, {
+    contentComponent: CustomDrawerComponent
+  });
 const MyApp = createAppContainer(AppDrawerNavigator);
 
 const styles = StyleSheet.create({
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>HomeScreen :3!</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.props.navigation.openDrawer()}
+        >
+          <Text style={styles.buttonText}>Open Drawer</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -18,4 +24,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    position: 'absolute',
+    top: 50,
+    left: 0,
+    width: 150,
+    height: 50,
+    backgroundColor: '#f39c12',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'white'
+  }
 });
